a299a4f936d670fc0f569b85cf0e527de2bf468a
Adedeji Rotibi <dayjeerow7@gmail.com>
2020-02-24 17:18:51 +0100

    Done base

--- lib/viewmodels/startup_view_model.dart
+++ lib/viewmodels/startup_view_model.dart
@@ -3,24 +3,14 @@ import '../locator.dart';
 import '../services/navigation_service.dart';
 import '../viewmodels/base_model.dart';
 
-// import '../ui/views/explore/index.dart';
+import '../ui/views/intro/splash_screen.dart';
 
 class StartUpViewModel extends BaseModel {
-  // final AuthenticationService _authenticationService =
-  //     locator<AuthenticationService>();
   final NavigationService _navigationService = locator<NavigationService>();
 
   Future handleStartUpLogic() async {
-    // var hasLoggedInUser = false;
     Future.delayed(Duration(seconds: 3), () {
-      // _navigationService.navigateTo(ExplorePage.routeName, replace: true);
+      _navigationService.navigateTo(SplashScreen.routeName, replace: true);
     });
-
-    // var hasLoggedInUser = await _authenticationService.isUserLoggedIn();
-    // if (hasLoggedInUser) {
-    //   _navigationService.navigateTo(HomeViewRoute);
-    // } else {
-    //   _navigationService.navigateTo(LoginViewRoute);
-    // }
   }
 }
