d830961417d2373e13dde20fca0382f594940757
ibuku <babatundeibukun16@gmail.com>
2020-04-24 16:30:40 +0100

    API life

--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,3 @@
-const String BASE_URL = "https://sfscredit.herokuapp.com";
-const String API_BASE_URL = "https://sfscreditbackend.herokuapp.com/api/v1"; 
-const String PAYSTACK_PUBLIC_KEY = "pk_test_8ede7554506f42a3efd132d3d55e23054263b622";
+const String BASE_URL = "https://sfscredit.com";
+const String API_BASE_URL = "https://api.sfscredit.com/api/v1";
+const String PAYSTACK_PUBLIC_KEY = "pk_live_6047ef197e04d69bb36db60024011905cfb901fc";
